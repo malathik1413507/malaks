6d2ada11878bc39056b41b7a2f5740254554b478
malathik1413507 <malathi.kcse@ksrce.ac.in>
2017-06-06 15:13:35 +0530

    Add files via upload

---
+++ Reverse1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+public class Reverse_Number
+{
+	public static void main(String args[])
+	{
+	String str="1234";
+	System.out.println("Reverse string is" +str.reverse());
+	}
+}
