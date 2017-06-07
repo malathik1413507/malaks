00cb3e3b54174d61b2897e186994f3d69e9cadd4
malathik1413507 <malathi.kcse@ksrce.ac.in>
2017-06-07 10:08:15 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+package Example;
+import java.lang.*;
+import java.util.*;
+public class Conversion {
+	public static void main(String args[]){
+	Scanner input=new Scanner(System.in);
+	String name=input.next();
+	int result=Integer.parseInt(name);
+	System.out.println(result);
+	
+	}
+}
