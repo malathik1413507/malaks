521a110e0343aef81fcf0b04186fe2a85ca05966
malathik1413507 <malathi.kcse@ksrce.ac.in>
2017-07-15 17:25:59 +0530

    Add files via upload

---
+++ LongestSubstringPolin.java
@@ -0,0 +1,43 @@
+package examplecoding;
+
+import java.util.Scanner;
+
+public class LongestSubstringPolin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		String input=scan.next();
+		String output="";
+		for(int i=1;i<input.length();i++)
+		{
+			int s=input.charAt(i)-input.charAt(i-1);
+			if(s>0)
+			{
+				output=input.substring(i);
+				break;
+			}
+		}
+		if(output.length()==0)
+		{
+			System.out.println("no longest substring");
+		}
+		else
+		{
+			System.out.println("The longest substring is:"+output);
+		}
+
+		scan.close();
+		if(output.equals(input))
+		{
+			System.out.println("polindrome");
+		}
+		else
+		{
+			System.out.println("not a polindrome");
+		}
+
+
+	}
+
+}
