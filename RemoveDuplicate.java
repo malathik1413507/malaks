2c0eaa85f6d4702db5dce5025d3307fe7da9ec33
malathik1413507 <malathi.kcse@ksrce.ac.in>
2017-07-22 10:01:24 +0530

    Add files via upload

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,27 +1,28 @@
-package examplecoding;
+package Example;
 
-import java.util.Scanner;
+
 
 public class RemoveDuplicate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan=new Scanner(System.in);
-		String str=scan.next();
-		char ch;
-		String s="";
-		scan.close();
-		for(int i=0;i<str.length();i++)
-		{
-			ch=str.charAt(i);
-			if(ch!=' ')
-			{
-				s=s+ch;
-				str=str.replace(ch, ' ');
-			}
-			
-		}
-		System.out.println(s);
+//		Scanner scan=new Scanner(System.in);
+//		String str=scan.next();
+		 String s = "malayalam";
+	        String s2 = "";
+	        for (int i = 0; i < s.length(); i++) {
+	            Boolean found = false;
+	            for (int j = 0; j < s2.length(); j++) {
+	                if (s.charAt(i) == s2.charAt(j)) {
+	                    found = true;
+	                    break; //don't need to iterate further
+	                }
+	            }
+	            if (found == false) {
+	                s2 = s2.concat(String.valueOf(s.charAt(i)));
+	            }
+	        }
+	        System.out.println(s2);
 
 	}
 
